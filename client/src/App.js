f3bc10e5c71bb3a8eb63d91cfc6111e26e66df34
unknown <mhudyx@gmail.com>
2020-07-28 20:30:55 +0200

    Create a basic style for mobile interface

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import 'font-awesome/css/font-awesome.min.css';
 import './App.css';
 import { Navigation, Content, Footer } from './layouts'; 
 
--- client/src/layouts/Navigation/Navigation.js
+++ client/src/layouts/Navigation/Navigation.js
@@ -2,10 +2,21 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Navigation.css';
 
+const toggleMenu = () => {
+    document.querySelector(".fa-bars").classList.toggle("hidden");
+    document.querySelector(".fa-times").classList.toggle("hidden");
+}
+
 const Navigation = () => {
     return (
         <nav>
-            Navigation
+            <div className="menu-container">
+                <div className="menu-burger">
+                    <i className="fa fa-bars" aria-hidden="true" onClick={toggleMenu}></i>
+                    <i className="fa fa-times hidden" aria-hidden="true" onClick={toggleMenu}></i>    
+                </div>
+                <div className="menu-logo">ShopIt</div>
+            </div>
         </nav>
     )
 }
