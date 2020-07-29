2e50b23824c5d7670fab87456eada1e8d1df73d8
unknown <mhudyx@gmail.com>
2020-07-29 22:25:48 +0200

    Add a temporary data for the database

---
+++ server/data.js
@@ -0,0 +1,22 @@
+export default {
+    products: [
+        {
+            _id: '1',
+            name: 'Slim Shirt',
+            type: 'Men',
+            category: 'Shirts',
+            image: '/images/1.jpg',
+            price: 20,
+            brand: 'Nicke'
+        },
+        {
+            _id: '2',
+            name: 'Fit Shirt',
+            type: 'Men',
+            category: 'Shirts',
+            image: '/images/2.jpg',
+            price: 22,
+            brand: 'Adidaz'
+        }
+    ]
+}
