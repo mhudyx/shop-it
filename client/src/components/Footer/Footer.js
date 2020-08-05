a52ac37a80592b8fc59ea1e0f005eb66078aa4f2
unknown <mhudyx@gmail.com>
2020-08-05 18:56:08 +0200

    Rename layouts to components

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            MHDEV&copy;2020
+        </footer>
+    )
+}
+
+export default Footer; 
