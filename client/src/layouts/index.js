4a2563df142481c94fc19ee372bce56c4a790172
unknown <mhudyx@gmail.com>
2020-08-05 20:15:37 +0200

    Delete layouts

--- client/src/layouts/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Navigation } from './Navigation';
-export { default as Aside } from './Aside';
-export { default as Content } from './Content';
-export { default as Footer } from './Footer'; 
