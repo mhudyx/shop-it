665024b0077d90d5ef497c558ed3598b369fda52
unknown <mhudyx@gmail.com>
2020-09-25 20:41:08 +0200

    Add signin page

---
+++ client/src/pages/Signin/Signin.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { signin } from '../../actions/user.action';
+import './Signin.css';
+import { PromiseProvider } from 'mongoose';
+
+const SigninPage = (props) => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const userSignin = useSelector(state => state.userSignin);
+    const { loading, userInfo, error } = userSignin;
+
+    const dispatch = useDispatch();
+    useEffect(() => {
+        if(userInfo){
+           props.history.push("/");
+        }
+
+        return () => {
+            //
+        }
+    }, [userInfo]);
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        dispatch(signin(email, password));
+    }
+
+    return (
+        <div className="signin">
+            <div className="form">
+                <form onSubmit={submitHandler}>
+                    <ul className="form-container">
+                        <li>
+                            <h2>Sign In</h2>
+                        </li>
+                        <li>
+                            {loading && <div>Loading...</div>}
+                            {error && <div>{error}</div>}
+                        </li>
+                        <li>
+                            <label htmlFor="email">Email</label>
+                            <input type="email" name="email" id="email" onChange={(e) => setEmail(e.target.value)} />
+                        </li>
+                        <li>
+                            <label htmlFor="password">Password</label>
+                            <input type="password" id="password" name="password" onChange={(e) => setPassword(e.target.value)} />
+                        </li>
+                        <li>
+                            <button type="submit" className="button wide">Sign In</button>
+                        </li>
+                        <li className="aqua">
+                            New to ShopIt?
+                        </li>
+                        <li>
+                            <NavLink to="/register" className="button wide empty">Create your account!</NavLink>
+                        </li>
+                    </ul>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default SigninPage; 
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -3,3 +3,4 @@ export { default as WomenPage } from './Women';
 export { default as MenPage } from './Men';
 export { default as SalePage } from './Sale';
 export { default as AboutPage } from './About'; 
+export { default as SigninPage } from './Signin'; 
