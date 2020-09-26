be3a55815a4bf210f2ccb7363ae2eb763ce0049b
unknown <mhudyx@gmail.com>
2020-09-26 22:09:18 +0200

    Add actions for register

--- client/src/actions/user.action.js
+++ client/src/actions/user.action.js
@@ -1,5 +1,6 @@
 import {
-    USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS, USER_SIGNIN_FAILURE
+    USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS, USER_SIGNIN_FAILURE,
+    USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_REGISTER_FAILURE
 } from '../constans';
 
 import axios from 'axios';
@@ -13,5 +14,16 @@ export const signin = (email, password) => async (dispatch) => {
         Cookie.set('userInfo', JSON.stringify(data));
     } catch (error) {
         dispatch({ type: USER_SIGNIN_FAILURE, payload: error.message });
+    }
+}
+
+export const register = (name, email, password) => async (dispatch) => {
+    dispatch({ type: USER_REGISTER_REQUEST, payload: { name, email, password } });
+    try {
+        const { data } = await axios.post("/api/users/register", { name, email, password });
+        dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
+        Cookie.set('userInfo', JSON.stringify(data));
+    } catch (error) {
+        dispatch({ type: USER_REGISTER_FAILURE, payload: error.message });
     }
 }
