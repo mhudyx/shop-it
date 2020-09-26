4e39c2dc670314e2372a0d1b85fb080477b7f290
unknown <mhudyx@gmail.com>
2020-09-26 22:13:58 +0200

    Fix a typo in a message

--- server/server.js
+++ server/server.js
@@ -24,4 +24,4 @@ app.get('/api/products', (req, res) => {
     res.send(data.products);
 });
 
-app.listen(5000, () => { console.log("Server is running at at http://localhost:5000") })
+app.listen(5000, () => { console.log("Server is running at http://localhost:5000") });
