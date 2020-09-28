276f5d61f3c8bd83f8a95875204aeeb157c3fb07
unknown <mhudyx@gmail.com>
2020-09-28 23:01:44 +0200

    Add actions for save and delete product

--- client/src/actions/product.action.js
+++ client/src/actions/product.action.js
@@ -1,5 +1,7 @@
 import {
-    PRODUCTS_GET_REQUEST, PRODUCTS_GET_SUCCESS, PRODUCTS_GET_FAILURE
+    PRODUCTS_GET_REQUEST, PRODUCTS_GET_SUCCESS, PRODUCTS_GET_FAILURE,
+    PRODUCT_SAVE_REQUEST, PRODUCT_SAVE_SUCCESS, PRODUCT_SAVE_FAILURE,
+    PRODUCT_DELETE_REQUEST, PRODUCT_DELETE_SUCCESS, PRODUCT_DELETE_FAILURE
 } from '../constans/product.const';
 
 import axios from 'axios';
@@ -13,4 +15,45 @@ export const listProducts = () => async (dispatch) => {
     catch (error) {
         dispatch({ type: PRODUCTS_GET_FAILURE, payload: error.message });
     }
+}
+
+export const saveProduct = (product) => async (dispatch, getState) => {
+    try {
+        dispatch({ type: PRODUCT_SAVE_REQUEST, payload: product });
+        const { userSignin: { userInfo } } = getState();
+
+        if(!product._id) {
+            const { data } = await axios.post('/api/products', product, { 
+                headers: {
+                    'Authorization': 'Bearer ' + userInfo.token
+                }
+            });
+            dispatch({ type: PRODUCT_SAVE_SUCCESS, payload: data });
+        } else {
+            const { data } = await axios.put('/api/products/' + product._id, product, { 
+                headers: {
+                    'Authorization': 'Bearer ' + userInfo.token
+                }
+            });
+            dispatch({ type: PRODUCT_SAVE_SUCCESS, payload: data });
+        }
+        
+    } catch (error) {
+        dispatch({ type: PRODUCT_SAVE_FAILURE, payload: error.message });
+    }
+}
+
+export const deleteProduct = (productId) => async (dispatch, getState) => {
+    try {
+        const { userSignin: { userInfo } } = getState();
+        dispatch({ type: PRODUCT_DELETE_REQUEST, payload: productId });
+        const { data } = await axios.delete('/api/products/' + productId, {
+            headers: {
+                Authorization: 'Bearer ' + userInfo.token
+            }
+        });
+        dispatch({ type: PRODUCT_DELETE_SUCCESS, payload: data, success: true });
+        } catch (error) {
+            dispatch({ type: PRODUCT_DELETE_FAILURE, payload: error.message });
+        }
 }
