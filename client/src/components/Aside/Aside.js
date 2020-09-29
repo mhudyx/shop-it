477e9f97dbd57465db7d570af2508f64b90def16
unknown <mhudyx@gmail.com>
2020-09-29 19:21:47 +0200

    Small changes for buttons in aside

--- client/src/components/Aside/Aside.js
+++ client/src/components/Aside/Aside.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Aside.css';
+import { useSelector } from 'react-redux';
 
 const Aside = () => {
+
+    const userSignin = useSelector(state => state.userSignin);
+    const { userInfo } = userSignin;
+
     return (
         <aside>
             <div className="aside-container">
                 <div className="aside-account">
-                    <NavLink to="#" className="button mobile empty">Sign In</NavLink>
+                    { 
+                        userInfo ? 
+                        <>
+                        <NavLink to="/profile" className="button mobile empty">Your Profile</NavLink>
+                        <NavLink to="/manage-product" className="button mobile empty">Manage</NavLink>
+                        </>
+                        :
+                        <NavLink to="/signin" className="button mobile empty">Sign In</NavLink>
+                    }
                     <NavLink to="#" className="button mobile ">Your Cart</NavLink>
                 </div>
                 <div className="aside-categories">
