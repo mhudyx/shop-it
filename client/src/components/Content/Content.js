4071c548a39b0aa966fae0ec37ba7a7931ee2a55
unknown <mhudyx@gmail.com>
2020-09-29 19:22:09 +0200

    Small changes in styles for content

--- client/src/components/Content/Content.js
+++ client/src/components/Content/Content.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavLink, Route, Switch } from 'react-router-dom';
 import './Content.css';
 
-import { HomePage, WomenPage, MenPage, SalePage, AboutPage, SigninPage, RegisterPage, ManageProductsPage } from '../../pages';
+import { HomePage, WomenPage, MenPage, SalePage, AboutPage, SigninPage, RegisterPage, ManageProductsPage, ProductPage } from '../../pages';
 
 const categoriesList = [
     {name: "Women", path: "/women"},
@@ -32,6 +32,7 @@ const Content = () => {
                     <Route path="/signin" component={SigninPage} />
                     <Route path="/register" component={RegisterPage} />
                     <Route path="/manage-product" component={ManageProductsPage} />
+                    <Route path="/product/:id" component={ProductPage} />
                 </Switch>
             </div> 
         </main>
