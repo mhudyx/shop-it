6077c722fb01b56d4f2b5a340f0aae6b9667c8b9
unknown <mhudyx@gmail.com>
2020-11-03 10:37:40 +0100

    Add rotue and model for pay for order

--- server/routes/order.route.js
+++ server/routes/order.route.js
@@ -1,3 +1,4 @@
+import e from 'express';
 import express from 'express';
 import Order from '../models/order.model.js';
 import { isAuth } from '../util.js';
@@ -6,7 +7,7 @@ const router = express.Router();
 
 router.post('/', isAuth, async (req,res) => {
     if(req.body.orderItems.length === 0) {
-        res.status(400).send({ msg: 'Cart is empty' });
+        res.status(400).send({ message: 'Cart is empty' });
     } else {
         const order = new Order({
             orderItems: req.body.orderItems,
@@ -18,7 +19,29 @@ router.post('/', isAuth, async (req,res) => {
             user: req.user._id,
         });
         const createdOrder = await order.save();
-        res.status(201).send({ msg: 'New order created', order: createdOrder });
+        res.status(201).send({ message: 'New order created', order: createdOrder });
+    }
+})
+
+router.get('/:id', isAuth, async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if(order) {
+        res.send(order);
+    } else {
+        res.status(404).send({ message: 'Order not found' });
+    }
+})
+
+router.put('/:id/pay', isAuth, async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if(order) {
+        order.isPaid = true;
+        order.paidAt = Date.now();
+        order.paymentResult = { status: req.body.status };
+        const updatedOrder = await order.save();
+        res.send({ message: 'Order Paid', order: updatedOrder });
+    } else {
+        res.status(404).send({ message: 'Order not found' });
     }
 })
 
